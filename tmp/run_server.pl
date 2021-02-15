5967cd37b767ce62308a5dd1de8e21afb079ea13
Wouter Beek <wouter@triply.cc>
2021-02-15 12:29:49 +0100

    CLEAN: Removed demo server.

--- tmp/run_server.pl
+++
@@ -1,4 +0,0 @@
-:- use_module(library(http/http_unix_daemon)).
-:- use_module(library(http/thread_httpd)).
-
-:- use_module(gis_server).
--- tmp/run_server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-swipl -q run_server.pl --port=3020 --interactive --workers=16
